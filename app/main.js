5376927626cce9b9ef9ab8631e1c9349c92fd213
BatuhanCaglar <fbmanbc@gmail.com>
2024-11-13 13:58:39 -0500

    bruh

--- app/main.js
+++ app/main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+async function getheros() {
+  try {
+    const response = await fetch();
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console(data.data);
+      data.data.forEach((agent) => console.log(agent.displayName));
+    }
+  } catch (error) {
+    alert("woah buddy");
+  }
+}
+
+getdata();
